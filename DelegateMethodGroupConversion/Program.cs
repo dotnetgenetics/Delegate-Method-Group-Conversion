d6393d3e8090f8dbc60de0e9abf707c2d54eb107
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-19 22:54:43 +0800

    Add project files.

---
+++ DelegateMethodGroupConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateMethodGroupConversion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> studentOne = EvaluateStudent;
+            studentOne("Greg Esguerra");
+            Action<string> studentTwo = EvaluateStudent;
+            studentTwo("Michelle Esguerra");
+
+            //studentOne and studentTwo refer to same delegate instance
+            Console.WriteLine(Object.ReferenceEquals(studentOne, studentTwo));
+        }
+
+        static void EvaluateStudent(string name)
+        {
+            Console.WriteLine($"Student for evaluation is {name}");
+        }
+    }
+}
